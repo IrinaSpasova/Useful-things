6e9b340bb6c7b75e56b8405d294ee274a0b2e8fb
IrinaSpasova <77418931+IrinaSpasova@users.noreply.github.com>
2021-06-08 15:13:09 +0300

    Update factoriel.js

--- JS/factoriel.js
+++ JS/factoriel.js
@@ -1,4 +1,16 @@
+function data(input){
+let num = Number(input[0]);
 
+/*LOGIC*/
+let sum = 1;
+for (i=1; i<=num; i++){
+sum *= i;
+}
+console.log(sum);
+
+
+}
+data(["8"])
 
 /* произволно число на степен
 
